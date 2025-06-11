364e3bb8177f28f20746904902199647e407e5e3
Sanjana Patil <155346868+sanjanapatil-js@users.noreply.github.com>
2025-06-11 19:34:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,17 +11,17 @@ var express    = require("express"),
     User       = require("./models/user"),
     seedDB     = require("./seeds");
  
-//Requiring routes
+
 var commentRoutes  = require("./routes/comments"),
     bachelorRoutes = require("./routes/bachelors"),
     indexRoutes    = require("./routes/index");
 
     
-// seedDB(); //seed the database
+
 mongoose.connect("mongodb://localhost/matri_monial", {useNewUrlParser: true});
 mongoose.connection
-    .once("open", () => console.log("Connected"))  //If connection is successful
-    .on("eror", error => {                          //If any error occurs
+    .once("open", () => console.log("Connected"))  
+    .on("eror", error => {                          
         console.log("Your Error: ",error);
     });
 
@@ -31,7 +31,7 @@ app.use(express.static(__dirname+"/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
 
-//PASSPORT CONFIGURATION
+
 app.use(require("express-session")({
     secret: "I will make you proud oneday. ",
     resave: false,
@@ -43,7 +43,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next){           //This is the middleware for every single route
+app.use(function(req, res, next){           
     res.locals.currentUser = req.user;
     res.locals.error     = req.flash("error");
     res.locals.success   = req.flash("success");
